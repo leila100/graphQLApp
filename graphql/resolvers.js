3b8be1ddf7dd00ad7ce20e2e7a6af06376ac9ac6
Leila <nb.leila10@gmail.com>
2019-05-10 13:11:59 -0500

    Add error handling using validator and formatError(err)

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,24 @@
 const bcrypt = require("bcryptjs");
+const validator = require("validator");
 
 const User = require("../models/user");
 
 module.exports = {
   createUser: async function(args, req) {
     const { email, name, password } = args.userInput;
+    const errors = [];
+    if (!validator.isEmail(email)) {
+      errors.push({ message: "Email is invalid!" });
+    }
+    if (validator.isEmpty(password) || !validator.isLength(password, { min: 5 })) {
+      errors.push({ message: "Password should be at least 5 characters long!" });
+    }
+    if (errors.length > 0) {
+      const error = new Error("Invalid Input.");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
     const existingUser = await User.findOne({ email: email });
     if (existingUser) {
       const error = new Error("User exists already!");
